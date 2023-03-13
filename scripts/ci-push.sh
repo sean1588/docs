41cac21c3aeec383fdab4497297e484e5f49f081
Sean Holung <sean.holung@gmail.com>
2023-03-13 16:42:20 -0700

    tst

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -5,7 +5,7 @@ set -o errexit -o pipefail
 source ./scripts/ci-login.sh
 
 ./scripts/build-site.sh
-./scripts/sync-and-test-bucket.sh
+# ./scripts/sync-and-test-bucket.sh
 
 # Wait for in-progress jobs to complete before proceeding.
 node ./scripts/await-in-progress.js
