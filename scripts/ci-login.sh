839612978a6ffa844519328a2f4c1e4c2afe311f
Sean Holung <sean.holung@gmail.com>
2023-03-13 16:30:40 -0700

    testing (#1)

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -o errexit -o pipefail
+# set -o errexit -o pipefail
 
-echo "Assuming the production CI role..."
-eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
+# echo "Assuming the production CI role..."
+# eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
 
-echo "Selecting the pulumi/production stack"
+# echo "Selecting the pulumi/production stack"
 pulumi login
-pulumi -C infrastructure stack select pulumi/production
+pulumi -C infrastructure stack select pulumi/www-staging
--- scripts/run-pulumi.sh
+++ scripts/run-pulumi.sh
@@ -16,15 +16,17 @@ export PULUMI_ACTION=${1}
 
 case ${PULUMI_ACTION} in
     preview)
+        pulumi stack select "pulumi/www-staging"
         pulumi -C infrastructure preview
         ;;
     update)
         # Given how frequently we update the CloudFront distribution, and how easy it can
         # be for our checkpointed CloudFront Etag to fall out of sync with what's current,
         # we refresh the distribution on every update.
+        pulumi stack select "pulumi/www-staging"
         pulumi -C infrastructure refresh -t "urn:pulumi:production::www.pulumi.com::aws:cloudfront/distribution:Distribution::cdn" --yes
 
-        pulumi -C infrastructure up --yes
+        pulumi -C infrastructure preview
         ;;
     *)
         echo "Unknown action '${PULUMI_ACTION}'"
